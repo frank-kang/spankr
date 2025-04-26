e53da321ea659d4a10deb3ef30b6960512ef220c
Frank Kang <holycow001@gmail.com>
2025-04-26 17:56:35 +0000

    in progress

--- database/spankr.sql
+++ database/spankr.sql
@@ -1,11 +1,3 @@
-set client_min_messages to warning;
-
--- DANGER: this is NOT how to do it in the real world.
--- `drop schema` INSTANTLY ERASES EVERYTHING.
-drop schema "public" cascade;
-
-create schema "public";
-
 CREATE TABLE "user" (
   "id" serial PRIMARY KEY,
   "firstName" text NOT NULL,
