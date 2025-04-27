837e7970d877f3586081d1a9b4d5d34a494c1533
Frank Kang <holycow001@gmail.com>
2025-04-27 18:12:28 +0000

    seeded data

--- database/data.sql
+++ database/data.sql
@@ -1,11 +1,41 @@
--- Use SQL insert statements to add any
--- starting/dummy data to your database tables
+-- This file contains the SQL commands to populate the database with initial data.
 
--- EXAMPLE:
+insert into "genders"
+  ("genderName")
+    values
+  ('male'),
+  ('female');
 
---  insert into "todos"
---    ("task", "isCompleted")
---    values
---      ('Learn to code', false),
---      ('Build projects', false),
---      ('Get a job', false);
+insert into "handedness"
+  ("handedNessName")
+    values
+  ('right'),
+  ('left');
+
+insert into "skills"
+  ("skillName", "skillDescription")
+    values
+  ('NTRP 2.5', 'Beginner - Has limited experience and is still working primarily on getting the ball into play.'),
+  ('NTRP 3.0', 'Improving - Has a fair amount of experience and is working to develop more consistent stroke mechanics.'),
+  ('NTRP 3.5', 'Intermediate - Has acquired the basic skills needed to play tennis.'),
+  ('NTRP 4.0', 'Advanced - Has dependable strokes and is developing control over depth and direction.'),
+  ('NTRP 4.5', 'Pro - Has good shot anticipation and frequently has a well-developed game plan.'),
+  ('NTRP 5.0', 'Expert - Has a complete game and can hit all the shots with power and control.');
+
+insert into "playTypes"
+  ("playTypeName")
+    values
+  ('singles'),
+  ('doubles'),
+  ('both');
+
+insert into "users"
+   ("firstName", "lastName", "email","longDescription", "zipCode", "password","skillId","playTypeId","genderId","handedNessId")
+   values
+   ('Frank', 'Kang', 'frankkang@rocketmail.com', 'Plays offensive all court game', 92026, 'tennis', 4, 1, 1, 1),
+   ('Tom', 'Brown', 'tombrown@rocketmail.com', 'Plays defensive baseline game', 92025, 'tennis', 5, 1, 1, 1),
+   ('Mary', 'Larkin', 'marylarkin@rocketmail.com', 'Plays defensive all court game', 92024, 'tennis', 4, 1, 2, 2),
+   ('Barry', 'Dirt', 'barydirt@rocketmail.com', 'Plays serve and volley game', 92023, 'tennis', 4, 3, 1, 1),
+   ('John', 'Rosin', 'johnrosin@rocketmail.com', 'Plays defensive all court game', 92021, 'tennis', 4, 1, 1, 1),
+   ('Brian', 'Banks', 'brianbanks@rocketmail.com', 'Plays offensive all court game', 92022, 'tennis', 4, 1, 1, 1),
+   ('Jennie', 'Jones', 'jennyjones@rocketmail.com', 'Plays defensive baseline game', 92024, 'tennis', 4, 3, 2, 1);
--- database/schema.sql
+++ database/schema.sql
@@ -6,32 +6,46 @@ drop schema "public" cascade;
 
 create schema "public";
 
-CREATE TABLE "user" (
-  "id" serial PRIMARY KEY,
-  "firstName" text NOT NULL,
-  "lastName" text NOT NULL,
-  "email" text NOT NULL,
-  "longDescription" text NOT NULL,
-  "zipCode" integer NOT NULL,
-  "password" text NOT NULL,
-  "skillId" integer NOT NULL,
-  "playTypeId" integer NOT NULL,
-  "createdAt" timestamp,
-  "gender" text NOT NULL,
-  "handedNessId" text NOT NULL
-);
+CREATE TABLE "genders" (
+  "genderId" serial PRIMARY KEY,
+  "genderName" text NOT NULL);
 
-CREATE TABLE "skill" (
+CREATE TABLE "handedness" (
+  "handedNessId" serial PRIMARY KEY,
+  "handedNessName" text NOT NULL);
+
+CREATE TABLE "skills" (
   "skillId" serial PRIMARY KEY,
   "skillName" text NOT NULL,
   "skillDescription" text NOT NULL
 );
 
-CREATE TABLE "playType" (
+CREATE TABLE "playTypes" (
   "playTypeId" serial PRIMARY KEY,
   "playTypeName" text NOT NULL
 );
 
-ALTER TABLE "user" ADD FOREIGN KEY ("playTypeId") REFERENCES "playType" ("playTypeId");
+CREATE TABLE "users" (
+  "id" serial PRIMARY KEY,
+  "firstName" text,
+  "lastName" text,
+  "email" text,
+  "longDescription" text,
+  "zipCode" integer,
+  "password" text,
+  "skillId" integer,
+  "playTypeId" integer,
+  "createdAt" timestamp,
+  "genderId" integer,
+  "handedNessId" integer
+);
 
-ALTER TABLE "user" ADD FOREIGN KEY ("skillId") REFERENCES "skill" ("skillId");
+
+
+ALTER TABLE "users" ADD FOREIGN KEY ("playTypeId") REFERENCES "playTypes" ("playTypeId");
+
+ALTER TABLE "users" ADD FOREIGN KEY ("skillId") REFERENCES "skills" ("skillId");
+
+ALTER TABLE "users" ADD FOREIGN KEY ("handedNessId") REFERENCES "handedness" ("handedNessId");
+
+ALTER TABLE "users" ADD FOREIGN KEY ("genderId") REFERENCES "genders" ("genderId");
