b474a90c89a71717bd9da5643f5ead1f3d7928fe
Frank Kang <holycow001@gmail.com>
2025-05-01 00:48:27 +0000

    updated routes in app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,18 @@
 import './App.css';
 import { Welcome } from './pages/Welcome';
+import { Routes, Route } from 'react-router-dom';
+import { Registration } from './pages/Registration';
+import { SignInForm } from './pages/SignInForm';
+import { UserProvider } from './components/UserContext';
 
 export default function App() {
   return (
-    <>
-      <Welcome />
-    </>
+    <UserProvider>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/registration" element={<Registration />} />
+        <Route path="/signin" element={<SignInForm />} />
+      </Routes>
+    </UserProvider>
   );
 }
