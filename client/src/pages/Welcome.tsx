d54aaf5dbde442af309991df75fb305b125ced8b
Frank Kang <holycow001@gmail.com>
2025-05-07 21:23:18 +0000

    almost finished

--- client/src/pages/Welcome.tsx
+++ client/src/pages/Welcome.tsx
@@ -1,10 +1,28 @@
 import tennisCourtVideo from '../public/videos/tennis-court.mp4';
+import { Link } from 'react-router-dom';
 
 export function Welcome() {
   return (
-    <div className="welcome">
-      <h1>Welcome!</h1>
-      <video src={tennisCourtVideo} autoPlay muted loop></video>
-    </div>
+    <>
+      <div className="video-wrapper">
+        <video
+          id="background-video"
+          src={tennisCourtVideo}
+          autoPlay
+          muted></video>
+        <div className="content">
+          <span>
+            <h1>
+              <Link to="/signin">Login</Link>{' '}
+            </h1>
+          </span>
+          <span>
+            <h1>
+              <Link to="/registration">Register</Link>
+            </h1>
+          </span>
+        </div>
+      </div>
+    </>
   );
 }
